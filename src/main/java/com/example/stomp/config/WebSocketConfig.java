74ea7a3c2ad6a7344f71d1bbb8c2c691b3484f8f
jung min jun <alswns7984@naver.com>
2022-03-23 22:48:43 +0900

    WebSocketConfig 추가

---
+++ src/main/java/com/example/stomp/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.example.stomp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // "/topic" 시작되는 메시지가 메시지 브로커로 라우팅되도록 정의
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
